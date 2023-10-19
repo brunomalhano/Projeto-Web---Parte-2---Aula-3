7b46915ea230950c2329519ccfc048098d57ad48
Bruno Malhano <malhano94@hotmail.com>
2023-10-19 01:13:42 -0300

    first commit

---
+++ MyTasks/js/firebase.js
@@ -0,0 +1,12 @@
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyB1K_I4fEAzqUVoVcyM67-fM5RTkLL8YHU",
+    authDomain: "nomedoprojeto-f47cd.firebaseapp.com",
+    projectId: "nomedoprojeto-f47cd",
+    storageBucket: "nomedoprojeto-f47cd.appspot.com",
+    messagingSenderId: "963134891586",
+    appId: "1:963134891586:web:bd12b21baec767b2c46fdb"
+  };
+  
+    // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
